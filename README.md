8e858eb0b3378624f578e0d70b4d026b253f8ea5
nelat <nela.todorovic@gmail.com>
2020-01-14 22:09:06 +0100

    Dodate kocke i animirano kretanje po platformi i rotacija

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 
 ## ☛ Uputstvo za igranje
 
-* Isključivanje igre moguće je pritiskom na taster 'Q'
-* Brzina kretanja kockice po platformi zavisi od nagiba platforme i podešava se pritiskom na ↑ i ↓
-* Cilj je da 'uhvatite' kockicu u trenutku kada se ona nalazi na rubu platforme
+* Igra se započinje pritiskom na taster <kbd>Space</kbd>
+* Igra se isključuje pritiskom na taster <kbd>Esc</kbd>
+* Pauziranje igre moguće je pritiskom na taster <kbd>P</kbd>
+* Brzina kretanja kockice po platformi zavisi od nagiba platforme i podešava se pritiskom na <kbd>↑</kbd> i <kbd>↓</kbd>
+* Cilj je da 'uhvatite' kockice odgovarajuće boje u trenutku kada se one nalaze na rubu platforme
+* Tasteri za hvatanje kockice na svakoj od 5 linija platforme su redom <kbd>Q</kbd><kbd>W</kbd><kbd>E</kbd><kbd>R</kbd><kbd>T</kbd>
+
 
 <br/>
 
--- src/main.c
+++ src/main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
 #define TIMER_ID 0
 #define TIMER_INTERVAL 30
 
-static int window_height, window_width;
 
+static int timer_active;
+static int window_height, window_width;
+static int angle;
+static int animation_parameter;
+static int speed;
+/*static float gravity;*/
+
+static void on_special(int key, int x, int y);
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display();
-/*static void on_timer(int id);*/
+static void on_timer(int id);
 
-int angle = 0;
+
 
 int main(int argc, char **argv)
 {
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
 
@@ -23,9 +32,16 @@ int main(int argc, char **argv)
     glutInitWindowPosition(200, 100);
     glutCreateWindow("Cube Hero");
 
+    animation_parameter = 0;
+    timer_active = 0;
+    angle = 0;
+    speed = 1;
+    /*gravity = sin(angle);*/
+
     glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
+    glutSpecialFunc(on_special);
 
     glClearColor(0, 0, 0.2, 0);
 
@@ -34,32 +50,136 @@ int main(int argc, char **argv)
 
     glutMainLoop();
 
+
     return 0;
 }
+
+
+
+static void on_timer(int value)
+{
+    if (value != 0)
+        return;
+
+    animation_parameter +=1;
+    glutPostRedisplay();
+
+    if (timer_active)
+        glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+}
+
+
+
+static void on_special(int key, int x, int y)
+{
+
+    switch (key) {
+        case GLUT_KEY_UP:
+	if (angle < 60 && timer_active) /*platform can be rotated in-between 0° and 60° and only if the game has started */
+	     {  
+		angle += 10;
+		/*speed *= (1+gravity);*/
+		speed += 0.5;
+	     }
+        break;
+
+	case GLUT_KEY_DOWN:
+	if (angle > 0 && timer_active)
+             {  
+		angle -= 10;
+	     }
+        break;
+    }
+}
+
+
+
+static void on_keyboard(unsigned char key, int x, int y)
+{
+   
+    switch (key) {
+    case 27:        /* escape  ---> exiting game */
+        exit(0);
+	break;
+
+    case 32:        /* space  ---> starting animation */
+       if (timer_active == 0) 
+        {
+            glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+            timer_active = 1;
+        }
+        break;
+
+    case 'p':      /* 'p'  ---> pausing animation */
+	timer_active = 0;
+	break;
+    }
+}
+
+
+
+static void on_reshape(int width, int height)
+{
+
+    window_width = width;
+    window_height = height;
+    glViewport(0, 0, window_width, window_height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, window_width/window_height, 1, 100);
+
+}
+
+
+
+void drawCubes()
+{ 
+ 
+   glPushMatrix();
+
+	glColor3f(1,1,0); 
+	glTranslatef(0,0.08,-0.75);
+	glScalef(0.06, 0.06, 0.06);
+	glutSolidCube(1);
+
+   glPopMatrix();
+
+   glPushMatrix();
+
+	glColor3f(0,1,0); 
+	glTranslatef(-0.2,0.08,-0.75);
+	glScalef(0.06, 0.06, 0.06);
+	glutSolidCube(1);
+
+   glPopMatrix();
+
+}
+
 
 void drawPlatformLines()
 {
 
  glDisable(GL_LIGHTING);
+ glLineWidth(2);
 
     glBegin(GL_LINES);
         glColor3f(0,1,0);
-        glVertex3f(-0.2,0.05,-0.75);  
+        glVertex3f(-0.2,0.05,-0.75);  /* horizontal green line, length is the length of the platform */
         glVertex3f(-0.2,0.05,0.75);
 
         glColor3f(1,0,0);
-        glVertex3f(-0.1,0.05,-0.75);  
+        glVertex3f(-0.1,0.05,-0.75);  /*red*/
         glVertex3f(-0.1,0.05,0.75);  
 
         glColor3f(1,1,0);
-        glVertex3f(0,0.05,-0.75); 
+        glVertex3f(0,0.05,-0.75);     /*yellow*/
         glVertex3f(0,0.05,0.75);
 
         glColor3f(0,0,1);
-        glVertex3f(0.1,0.05,-0.75); 
+        glVertex3f(0.1,0.05,-0.75);   /*blue*/
         glVertex3f(0.1,0.05,0.75);
 
-        glColor3f(1,0.6,0);
+        glColor3f(1,0.6,0);	      /*orange*/
         glVertex3f(0.2,0.05,-0.75); 
         glVertex3f(0.2,0.05,0.75);
 
@@ -68,11 +188,15 @@ void drawPlatformLines()
     glEnable(GL_LIGHTING);
 }
 
-void drawSpeaker()
+
+void drawSpeakers()
 {
-    
+
+    /*left speaker*/
     glPushMatrix();
+
         glTranslatef(-1,0,-1);
+
 	glColor3f(0, 0, 0);
 	glScalef(0.3, 0.7, 0.3);
 	glutSolidCube(1);
@@ -80,13 +204,15 @@ void drawSpeaker()
 	glColor3f(1, 0, 0);
 	glTranslatef(0,0.1,0.5);
 	glutSolidTorus(0.1, 0.2, 30, 30);
-	
 
     glPopMatrix();
 
 
+     /*right speaker*/
     glPushMatrix();
+
 	glTranslatef(1,0,-1);
+
 	glColor3f(0, 0, 0);
 	glScalef(0.3, 0.7, 0.3);
 	glutSolidCube(1);
@@ -94,15 +220,16 @@ void drawSpeaker()
 	glColor3f(1, 0, 0);
 	glTranslatef(0,0.1,0.5);
 	glutSolidTorus(0.1, 0.2, 30, 30);
-        
+      
     glPopMatrix();
+
 }
 
 
-void drawAxes()
+void drawAxes() /*used to check platform rotation*/
 {
 
- glDisable(GL_LIGHTING);
+    glDisable(GL_LIGHTING);
 
     glBegin(GL_LINES);
         glColor3f(1,0,0);
@@ -119,57 +246,56 @@ void drawAxes()
     glEnd();
 
     glEnable(GL_LIGHTING);
+
 }
+
 
 void drawPlatform()
 {
+
     glPushMatrix();
 	glColor3f(0, 0, 0);
 	glScalef(0.5, 0.1, 1.5);
 	glutSolidCube(1);
     glPopMatrix();
+
 }
 
-static void on_keyboard(unsigned char key, int x, int y)
-{
-    switch (key) {
-    case 's':
-    case 'S':
-        exit(0);
-    case 'w':
-	angle+=30;
-        break;
-    case GLUT_KEY_DOWN:
-   	/*TODO pomeranje platforme*/
-        break;
-    }
-}
 
-static void on_reshape(int width, int height)
-{
-    window_width = width;
-    window_height = height;
-    glViewport(0, 0, window_width, window_height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60, window_width/window_height, 1, 100);
-}
 
 static void on_display()
 {
+   
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-            0, 1, 1,
+            0, 1.2, 1.2,
             0, 0, 0,
-            0, 1, 0
+            0, 1, 0.99 /* just to be non-colinear to eye vector*/
         );
 
     /*drawAxes();*/
-   /* glRotatef(angle, 1,0,0);*/
-    drawPlatformLines();
-    drawPlatform();
-    drawSpeaker();
+  
+    drawSpeakers();
+
+    
+    glRotatef(angle, 1, 0, 0);
+
+    glPushMatrix();
+        glTranslatef(0, 0, animation_parameter/100.0*speed);
+        drawCubes();
+    glPopMatrix();
+
+    glPushMatrix();
+	drawPlatformLines();
+    glPopMatrix();
+
+    glPushMatrix();
+        drawPlatform();
+    glPopMatrix();
+
+
     glutSwapBuffers();
+
 }
